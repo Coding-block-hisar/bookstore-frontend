48843d7a127a426a2ccdaadbafaa7fde0248eb23
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-25 15:06:53 +0530

    Update Books.jsx

--- src/components/Book/Books.jsx
+++ src/components/Book/Books.jsx
@@ -6,12 +6,14 @@ import Book from "./Book";
 const URL = "https://bookstore-backend-cyan.vercel.app/books";
 
 const fetchHandler = async () => {
+  axios.defaults.withCredentials = true;
   return await axios.get(URL,{
   withCredentials: true, // Ensure credentials (cookies) are included
 }).then((res) => res.data);
 };
 const Books = () => {
   const [books, setBooks] = useState();
+  
   useEffect(() => {
     fetchHandler().then((data) => setBooks(data.books));
   }, []);
