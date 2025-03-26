859b0233e1ae7cf242243d4e2722c4504336b401
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-26 10:06:14 +0530

    Update Books.jsx

--- src/components/Book/Books.jsx
+++ src/components/Book/Books.jsx
@@ -7,9 +7,7 @@ const URL = "https://bookstore-backend-self-eight.vercel.app/books";
 
 const fetchHandler = async () => {
   axios.defaults.withCredentials = true;
-  return await axios.get(URL,{
-  withCredentials: true, // Ensure credentials (cookies) are included
-}).then((res) => res.data);
+  return await axios.get(URL).then((res) => res.data);
 };
 const Books = () => {
   const [books, setBooks] = useState();
