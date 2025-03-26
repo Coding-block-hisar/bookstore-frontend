7cfa409e50f25fc5344356af7d346e88ad2da7df
akash kumar <65843349+Coding-block-hisar@users.noreply.github.com>
2025-03-26 09:52:42 +0530

    Update Book.jsx

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -7,7 +7,7 @@ const Book = (props) => {
   const { _id, name, author, description, price, image } = props.book;
   const deleteHandler = async () => {
     await axios
-      .delete(`https://bookstore-backend-cyan.vercel.app/books/${_id}`)
+      .delete(`https://bookstore-backend-self-eight.vercel.app/books/${_id}`)
       .then((res) => res.data)
       .then(() => history("/books"));
   };
